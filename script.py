f606fee6b390a585b688072a1b32d432b719d064
PTS <76948129+PTSProgrammer@users.noreply.github.com>
2022-05-21 23:15:08 +0545

    Update script.py

--- script.py
+++ script.py
@@ -9,8 +9,8 @@ emails = e['Emails'].values
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 server.login(SenderAddress, password)
-msg = "Dear Partners, \n For your ease we have uploaded a tutorial on youtube about using our site as a partner.This video covers topics like how signup, signin, reset and change password, add packs and a lot more.You are now supposed to upload your packs from tommorrow onwards.Thanks \n Url: https://youtu.be/hWbj9VIwCJQ \n Sincerely, \n Developer"
-subject = "How to use our site as a partner tutorial uploaded!!"
+msg = "Your message "
+subject = "Your Subject "
 body = "Subject: {}\n\n{}".format(subject,msg)
 for email in emails:
     server.sendmail(SenderAddress, email, body)
